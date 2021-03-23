a7ed99cc9c6f4cb3aa73f538161a1fe16ca15413
Ranjan Sarpangala Venkatesh <ranjansv@gmail.com>
2021-03-23 06:28:26 -0700

    Commit writer code and xml, to be tested

---
+++ benchmarks/writer.h
@@ -0,0 +1,20 @@
+#include <adios2.h>
+
+class Writer
+{
+	adios2::IO io;
+	adios2::Engine writer;
+	adios2::Variable<double> var_array;
+	adios2::Variable<int> var_step;
+
+	size_t global_array_size;
+	size_t offset;
+	size_t local_size;
+
+	public:
+	Writer(adios2::IO, size_t);
+	void open(const std::string &fname);
+	void write(int step);
+	void close();
+};
+	
