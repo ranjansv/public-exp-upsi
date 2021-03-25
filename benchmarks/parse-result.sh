8279704e23e19d503d51bb11e791d350861038ac
Ranjan Sarpangala Venkatesh <ranjansv@gmail.com>
2021-03-25 07:55:55 -0700

    parse-result.sh: Make csvs per rank

--- benchmarks/parse-result.sh
+++ benchmarks/parse-result.sh
@@ -5,21 +5,22 @@ RESULT_DIR=$1
 
 source ${RESULT_DIR}/config.sh
 
-for DATASIZE in $GLOBAL_ARRAYSIZE_GB
+for NR in $PROCS
 do
     #Setup outfile for recording average total write time   
     mkdir -p $RESULT_DIR/csv/ 
-    OUTPUT_FILE="${RESULT_DIR}/csv/writetime-${DATASIZE}gb.csv"
-    echo "engine,$PROCS" > $OUTPUT_FILE
+    OUTPUT_FILE="${RESULT_DIR}/csv/writetime-${NR}ranks.csv"
+    echo "datasize,$ENGINE" > $OUTPUT_FILE
     sed -i 's/\s/,/g' $OUTPUT_FILE
 
-    for ENG_TYPE in $ENGINE
+    for DATASIZE in $GLOBAL_ARRAYSIZE_GB
     do
-        echo -n "$ENG_TYPE," >> $OUTPUT_FILE
-        for NR in $PROCS
+        echo -n "$DATASIZE" >> $OUTPUT_FILE
+
+        for ENG_TYPE in $ENGINE
         do
-             AVG_TOTAL_WRITE_TIME=`ls -1 $RESULT_DIR/${DATASIZE}gb/${ENG_TYPE}writers/${NR}ranks/writer*.log|xargs -L 1 tail -1|awk -F '\t' '{print $4}'| awk 'BEGIN{sum=0;} {sum+=$1} END{printf "%.f\n", sum/NR;}'`
-            echo -n "$AVG_TOTAL_WRITE_TIME," >> $OUTPUT_FILE
+             AVG_TOTAL_WRITE_TIME=`ls -1 $RESULT_DIR/${DATASIZE}gb/${ENG_TYPE}writers/${NR}ranks/writer*.log|xargs -L 1 tail -1|awk -F '\t' '{print $4}'| awk 'BEGIN{sum=0;} {sum+=$1} END{printf "%.f\n", sum/NR/1000;}'`
+            echo -n ",$AVG_TOTAL_WRITE_TIME" >> $OUTPUT_FILE
         done
         echo "" >> $OUTPUT_FILE
     done
