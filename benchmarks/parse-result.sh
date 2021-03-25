6f5ef7141f3cdd9a4ef359aa456aa412f7db4255
Ranjan Sarpangala Venkatesh <ranjansv@gmail.com>
2021-03-25 13:05:50 -0700

    parse-result.sh

--- benchmarks/parse-result.sh
+++ benchmarks/parse-result.sh
@@ -15,11 +15,12 @@ do
 
     for DATASIZE in $TOTAL_DATA_PER_RANK
     do
-        echo -n "$DATASIZE" >> $OUTPUT_FILE
+	    DATASIZE_IN_GB=`echo "scale=1; $DATASIZE/1024" | bc`
+        echo -n "$DATASIZE_IN_GB" >> $OUTPUT_FILE
 
         for ENG_TYPE in $ENGINE
         do
-             AVG_TOTAL_WRITE_TIME=`ls -1 $RESULT_DIR/${NR}ranks/${ENG_TYPE}writers/${DATASIZE}mb/writer*.log|xargs -L 1 tail -1|awk -F '\t' '{print $4}'| awk 'BEGIN{sum=0;} {sum+=$1} END{printf "%.f\n", sum/NR/1000;}'`
+             AVG_TOTAL_WRITE_TIME=`ls -1 $RESULT_DIR/${NR}ranks/${ENG_TYPE}writers/${DATASIZE}mb/writer*.log|xargs -L 1 tail -1|awk -F '\t' '{print $4}'| awk 'BEGIN{sum=0;} {sum+=$1} END{printf "%.2f\n", sum/NR/1000;}'`
             echo -n ",$AVG_TOTAL_WRITE_TIME" >> $OUTPUT_FILE
         done
         echo "" >> $OUTPUT_FILE
