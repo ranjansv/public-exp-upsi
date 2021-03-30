a219112907457d5359b0e6c3764ab107ba37b761
Ranjan Sarpangala Venkatesh <ranjansv@gmail.com>
2021-03-30 06:20:44 -0700

    reader.cpp

--- benchmarks/reader.cpp
+++ benchmarks/reader.cpp
@@ -9,19 +9,34 @@
 
 #include "timer.hpp"
 
-int main(int argc, char* argv[])
-{
-	//Init ADIOS
-	
-	//Open IO instance
-	
-	//Declare variables
-	
-	//Open Engine
+int main(int argc, char *argv[]) {
+  MPI_Init(&argc, &argv);
+  std::string engine_type = std::string(argv[1]);
+  size_t arr_size_mb = std::stoi(argv[2]);
+  int steps = std::stoi(argv[3]);
+  int rank, comm_size, wrank;
 
-        //Perform Reads	
-	while(true) {
-	}
+  MPI_Comm_rank(MPI_COMM_WORLD, &wrank);
 
-	return 0;
+  const unsigned int color = 2;
+  MPI_Comm comm;
+  MPI_Comm_split(MPI_COMM_WORLD, color, wrank, &comm);
+
+  MPI_Comm_rank(comm, &rank);
+  MPI_Comm_size(comm, &comm_size);
+  // Init ADIOS
+  adios2::ADIOS ad("adios2.xml", comm);
+
+  // Open IO instance
+  adios2::IO reader_io = ad.DeclareIO("SimulationOutput");
+
+  // Declare variables
+
+  // Open Engine
+
+  // Perform Reads
+  while (true) {
+  }
+
+  return 0;
 }
