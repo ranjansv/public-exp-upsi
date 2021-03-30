de99471ffddebf40bec497d00f981f6f3e9b6f75
Ranjan Sarpangala Venkatesh <ranjansv@gmail.com>
2021-03-30 05:22:21 -0700

    Init reader.cpp

---
+++ benchmarks/reader.cpp
@@ -0,0 +1,27 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <vector>
+
+#include <adios2.h>
+#include <mpi.h>
+
+#include "timer.hpp"
+
+int main(int argc, char* argv[])
+{
+	//Init ADIOS
+	
+	//Open IO instance
+	
+	//Declare variables
+	
+	//Open Engine
+
+        //Perform Reads	
+	while(true) {
+	}
+
+	return 0;
+}
