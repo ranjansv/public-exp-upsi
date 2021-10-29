1cdcd9fcbfbf5018ae65b1b53cee37ce224f2bfd
Ranjan Sarpangala Venkatesh <ranjansv@gatech.edu>
2021-10-28 20:28:13 -0500

    Removed stale files

--- benchmarks/print-perf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Print perf stats for a specific run
-
-DIR=$1
-RANK=$2
-DATASIZE=$3
-ENGINE=$4
-
-find results/$DIR/ -iname 'std*.log'|grep ${RANK}ranks|grep ${DATASIZE}mb|grep $ENGINE|xargs -L 1 tail -30
