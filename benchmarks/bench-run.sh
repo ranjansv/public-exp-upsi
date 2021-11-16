b12c0e8ba166152694940bd93fb1b62887cb66c3
Ranjan Sarpangala Venkatesh <ranjansv@gatech.edu>
2021-11-15 20:31:59 -0600

    bench-run.sh

--- benchmarks/bench-run.sh
+++ benchmarks/bench-run.sh
@@ -2,7 +2,7 @@
 #SBATCH -J upsi-bench           # Job name
 #SBATCH -o upsi-bench.o%j       # Name of stdout output file
 #SBATCH -e upsi-bench.e%j       # Name of stderr error file
-#SBATCH -p flex                 # Queue (partition) name
+#SBATCH -p normal               # Queue (partition) name
 #SBATCH -N 47                # Total # of nodes 
 #SBATCH -n 1316              # Total # of mpi tasks
 #SBATCH --ntasks-per-node=28
